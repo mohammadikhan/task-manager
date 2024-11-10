f8c389df3d8969210b8f57d39992870f20f60727
Mohammad Khan <mohammadikhan821@gmail.com>
2024-11-10 13:47:37 -0500

    Commit all modified files at this point

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Card from './Card';
+
+console.log("Server is up and running on localhost:3000");
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1> TaskManage </h1>
+      <Card name="Login" isLoggedIn="false"></Card>
+    </header>
+  
   );
 }
 
