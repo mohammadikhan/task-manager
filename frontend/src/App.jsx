9390f9723b3ee96db9610749a7811eeb1f08e3f3
Mohammad Khan <mohammadikhan821@gmail.com>
2024-11-12 13:15:47 -0500

    Update bulk of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,41 @@
-import './index.css';
+import './styles/index.css'
 import Login from './components/Login';
 import Register from './components/Register';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
 
 console.log("Server is up and running on localhost:3000");
 
 function App() {
   return (
-    <>
-    <header>
-      <h1> TaskManage </h1>
-      
-      {/* <Login/> */}
-      
-    </header>
-    <Login/>
-    <Register/>
-    </>
     
+    <div className='body'>
+      <TypeAnimation
+        sequence={[
+          'Plan, track, and achieve your goals.',
+          1000,
+          'Prioritze. Organize. Succeed.',
+          1000,
+          'Break it down into tasks!',
+          1000,
+          'What will you finish today?'
+          
+        ]}
+        wrapper="span"
+        speed={50}
+        className='animation'
+        repeat={Infinity}
+      />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/'></Route>
+            <Route path='/login' element={<Login/>} />
+            <Route path='/register' element={<Register/>} />
+          </Routes>
+        </BrowserRouter>
+     
+    </div>
     
-  
   );
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,30 +1,37 @@
 
 import React from "react";
-
+import { Link } from "react-router-dom";
+import laptop from "../images/laptop.jpeg";
 
 function Login() {
 
    
     return(
 
-        <div className="card">
-            <h2>Login</h2>
-            <div>
-                <input placeholder="Username"></input>
+        <div className="login-container">
+        <div className="login-card">
+            <div className="login-left">
+                <h2 className="login-title">Welcome Back!</h2>
+                <p> Resume Managing Your Workflow Effortlessly. </p>
             </div>
-            <div>
-                <input placeholder="Password"></input>
+            <div className="login-right">
+                <h2 className="login-title">Login</h2>
+                <form className="login-form">
+                    <div className="input-group">
+                        <input type="email" placeholder="Email" className="login-input" required />
+                    </div>
+                    <div className="input-group">
+                        <input type="password" placeholder="Password" className="login-input" required />
+                    </div>
+                    <Link to="/forgot-password" className="forgot-password-link">Forgot password?</Link>
+                    <button className="login-button" type="submit">Log In</button>
+                </form>
+                <p className="signup-prompt">
+                    Don’t have an account? <Link to="/register" className="signup-link">Sign up</Link>
+                </p>
             </div>
-            <p></p>
-                <button className="button" name="Sign In" type="submit">Sign In</button>
-            <p>Don't have an account?</p>
-            <div>                
-                <button className="button">Sign Up</button>
-            </div>
-            
-            
-            
         </div>
+    </div>
     )
 
 }
