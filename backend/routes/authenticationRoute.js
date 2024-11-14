6ae62b4da6e0f246b1a21f51e564742f2a8861a0
Mohammad Khan <mohammadikhan821@gmail.com>
2024-11-14 12:53:33 -0500

    Update package.json

--- backend/routes/authenticationRoute.js
+++ backend/routes/authenticationRoute.js
@@ -1,12 +1,19 @@
 import express from 'express';
 
-const express = require("express");
+const app = express;
 const router = express.Router();
 
 const {login, register} = require("../controllers/authenticationController");
 
-router.post('/login', login);
-router.post('/register', register);
+// router.post('/login', login);
+// router.post('/register', register);
 
-module.exports = router;
+// module.exports = router;
 
+app.get("/login", (req, res) => {
+    res.send("Server is ready");
+});
+
+app.listen(3000, () => {
+    console.log("SERVER START AT http://localhost:3000");
+})
